836163339e2c25680ad435d492c3f273261fe6ad
bagasrizq <bagas.rizqsokya@gmail.com>
2023-10-17 22:49:38 +0700

    add : pagination users

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -14,6 +14,35 @@ router.get('/allusers', (req, res) => {
       });
 });
 
+router.get('/pagination', (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const perPage = parseInt(req.query.perPage) || 10;
+
+  const offset = (page - 1) * perPage;
+
+  pool.query(
+      'SELECT * FROM users OFFSET $1 LIMIT $2', [offset, perPage], (error, results) => {
+          if (error) {
+              throw error;
+          }
+          res.json(results.rows);
+      });
+});
+
+// ambil data dengan pagination (limit 10 movies) => gimana caranya biar bisa dynamic page dan limitnya
+router.get('/paginate', (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const offset = (page - 1) ;
+
+  pool.query(
+      `SELECT * FROM users OFFSET $1 ${ req.query.limit ? 'LIMIT ' + req.query.limit : '' } `, [offset], (error, results) => {
+          if (error) {
+            throw error;
+          }
+          res.json(results.rows);
+        });
+});
+
 router.post('/login', (req, res) => {
     pool.query(
       `SELECT * FROM users WHERE email = $1 AND password = $2`,
